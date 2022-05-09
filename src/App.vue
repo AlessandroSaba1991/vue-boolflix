9390217fa66979cc373c0ae2a6ecf6879b0701a6
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-05-09 19:43:17 +0200

    aggiuntqa milestone 3

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
         <img src="@/assets/img/boolflix.png" alt="logo boolflix" />
       </div>
       <form action="#" method="get" @submit.prevent="searchFilm">
-        <label for="search" class="text-white"
-          >Inserisci la tua ricerca:
+        <label for="search" class="text-white">
+          Inserisci la tua ricerca:
         </label>
         <input type="text" name="search" id="search" v-model="searchText" />
         <button type="submit">Cerca</button>
@@ -15,28 +15,32 @@
     <main>
       <ul>
         <li v-for="(film, index) in films" :key="film.id">
-          <h3>{{ film.title }}</h3>
-          <p>{{ film.original_title }}</p>
           <img
-            :src="'https://flagcdn.com/32x24/' + flags(index) + '.png'"
+            :src="'https://image.tmdb.org/t/p/w185/' + film.poster_path"
+            :alt="film.title"
+          />
+          <h3>{{ film.title || film.name }}</h3>
+          <p>{{ film.original_title || film.original_name }}</p>
+          <img
+            :src="'https://flagcdn.com/32x24/' + flagsFilm(index) + '.png'"
             alt=""
           />
 
-          <p>{{ film.vote_average }}</p>
+          <p style="color:gold">
+            <font-awesome-icon 
+              v-for="n in starSystemSolid(index)"
+              :key="n"
+              icon="fa-solid fa-star"
+            />
+            <font-awesome-icon 
+              v-for="number in starSystemRegular(index)"
+              :key="'r' + number"
+              icon="fa-regular fa-star"
+            />
+          </p>
         </li>
       </ul>
-      <ul>
-        <li v-for="(serie, index) in series" :key="serie.id">
-          <h3>{{ serie.name }}</h3>
-          <p>{{ serie.original_name }}</p>
-          <img
-            :src="'https://flagcdn.com/32x24/' + flags(index) + '.png'"
-            alt=""
-          />
-
-          <p>{{ serie.vote_average }}</p>
-        </li>
-      </ul>
+      
     </main>
   </div>
 </template>
@@ -50,31 +54,57 @@ export default {
     return {
       searchText: "",
       films: null,
-      series:null
+      starSolid: 0,
+      starRegular: 0,
     };
   },
   methods: {
     searchFilm() {
-      const requestLinkFilms = axios.get(`https://api.themoviedb.org/3/search/movie?api_key=f3afd7059da19eddb0348a3bc5186e80&language=it-IT&query=${this.searchText}&page=1&include_adult=false`)
-      const requestLinkSeries = axios.get(`https://api.themoviedb.org/3/search/tv?api_key=f3afd7059da19eddb0348a3bc5186e80&language=it-IT&query=${this.searchText}&page=1&include_adult=false`)
+      const requestLinkFilms = axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=f3afd7059da19eddb0348a3bc5186e80&language=it-IT&query=${this.searchText}&page=1&include_adult=false`
+      );
+      const requestLinkSeries = axios.get(
+        `https://api.themoviedb.org/3/search/tv?api_key=f3afd7059da19eddb0348a3bc5186e80&language=it-IT&query=${this.searchText}&page=1&include_adult=false`
+      );
       axios.all([requestLinkFilms, requestLinkSeries]).then(
         axios.spread((...responses) => {
-          this.films = responses[0].data.results;
-          this.series = responses[1].data.results
+          this.films = [...responses[0].data.results,...responses[1].data.results];
           this.searchText = "";
-          
         })
       );
     },
-    flags(index) {
+    flagsFilm(index) {
       if (this.films[index].original_language === "en") {
-        this.films[index].original_language = "gb"
+        this.films[index].original_language = "gb";
       } else if (this.films[index].original_language === "ja") {
         this.films[index].original_language = "jp";
       } else if (this.films[index].original_language === "el") {
         this.films[index].original_language = "gr";
       }
       return this.films[index].original_language;
+    },
+    
+    starSystemSolid(index) {
+      const numberStar = parseInt(this.films[index].vote_average / 2).toFixed(0);
+      const max = 5;
+     let starSolid = 0     
+      for (let i = 0; i < max; i++) {
+        if (parseInt(numberStar) >= i) {          
+          starSolid += 1;
+        } 
+      }
+      return starSolid
+    },
+    starSystemRegular(index) {
+      const numberStar = parseInt(this.films[index].vote_average / 2).toFixed(0);
+      const max = 5;
+     let starRegular = 0     
+      for (let i = 0; i < max; i++) {
+        if (parseInt(numberStar) < i) {          
+          starRegular += 1;
+        } 
+      }
+      return starRegular
     },
   },
   components: {},
--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/* Font Awesome icons*/
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons 👇 QUI AGGIUNGI LE ICONE che vuoi usare in camelCase */
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library 👇 QUI AGGIUNGI IL NOME DELL'ICONA in camelCase*/
+library.add(fasStar,farStar) 
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 const bootstrap = require('bootstrap')
 Vue.use(bootstrap)
 Vue.config.productionTip = false
