1f0eed51dab115e6c3fbcde06a88d82014cf02db
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-05-10 12:43:21 +0200

    add milestone 4

--- src/App.vue
+++ src/App.vue
@@ -13,33 +13,45 @@
       </form>
     </header>
     <main>
-      <ul>
-        <li v-for="(film, index) in films" :key="film.id">
-          <img
-            :src="'https://image.tmdb.org/t/p/w185/' + film.poster_path"
-            :alt="film.title"
-          />
-          <h3>{{ film.title || film.name }}</h3>
-          <p>{{ film.original_title || film.original_name }}</p>
-          <img
-            :src="'https://flagcdn.com/32x24/' + flagsFilm(index) + '.png'"
-            alt=""
-          />
+      <div class="container-fluid">
+        <div class="row justify-content-center g-1">
+          <div class="col_" v-for="(film, index) in films" :key="film.id" >
+            <div class="card_film">
+              <div class="img" >
+                <img
+                  :src="'https://image.tmdb.org/t/p/w342' + film.poster_path"
+                  :alt="film.title"
+                />
+              </div>
+              <div  class="description_film">
+                <p><strong>Titolo: </strong> {{ film.title || film.name }}</p>
+                <p><strong>Titolo originale: </strong>{{ film.original_title || film.original_name }}</p>
+                <img
+                  :src="
+                    'https://flagcdn.com/32x24/' + flagsFilm(index) + '.png'
+                  "
+                  alt=""
+                />
 
-          <p style="color: gold">
-            <font-awesome-icon
-              v-for="n in starSystem(index)"
-              :key="n"
-              icon="fa-solid fa-star"
-            />
-            <font-awesome-icon
-              v-for="number in maxStars - starSystem(index)"
-              :key="'r' + number"
-              icon="fa-regular fa-star"
-            />
-          </p>
-        </li>
-      </ul>
+                <p style="color: gold">
+                  <strong class="text-white">Voto: </strong>
+                  <font-awesome-icon
+                    v-for="n in starSystem(index)"
+                    :key="n"
+                    icon="fa-solid fa-star"
+                  />
+                  <font-awesome-icon
+                    v-for="number in maxStars - starSystem(index)"
+                    :key="'r' + number"
+                    icon="fa-regular fa-star"
+                  />
+                </p>
+                <p><strong>Overview: </strong>{{film.overview}}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -54,9 +66,11 @@ export default {
       searchText: "",
       films: null,
       maxStars: 5,
+      
     };
   },
   methods: {
+    
     searchFilm() {
       const requestLinkFilms = axios.get(
         `https://api.themoviedb.org/3/search/movie?api_key=f3afd7059da19eddb0348a3bc5186e80&language=it-IT&query=${this.searchText}&page=1&include_adult=false`
@@ -86,14 +100,7 @@ export default {
     },
 
     starSystem(index) {
-      const numberStar = Math.ceil(this.films[index].vote_average / 2);
-      let starSolid = 0;
-      for (let i = 0; i < this.maxStars; i++) {
-        if (parseInt(numberStar) > i) {
-          starSolid += 1;
-        }
-      }
-      return starSolid;
+      return Math.ceil(this.films[index].vote_average / 2);
     },
   },
   components: {},
@@ -108,5 +115,44 @@ header {
   align-items: center;
   background-color: rgb(26, 24, 24);
   padding: 1rem;
+}
+main{
+  background-color: gray;
+  padding: 3rem 1rem;
+}
+.col_{
+  width: fit-content;
+}
+.card_film{
+  overflow-y: auto;
+  width: 342px;
+  height: 513px;
+  border: 1px solid white;
+  background-color: black;
+  color: white;
+  .img{
+    img{
+      height: 100%;
+      width: 100%;
+    }
+
+  }
+  .description_film{
+    
+    padding: 2rem 0;
+    display: none;
+    p,
+    img{
+      margin-top: 0.25rem;
+      margin-bottom: 0;
+    }
+    
+  }
+}
+.card_film:hover .img{
+  display: none;
+}
+.card_film:hover .description_film{
+  display: block;
 }
 </style>
